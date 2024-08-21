07122f589a9a8936b8b204ffd2eb06ec27349266
Shan D. Wanigasuriya <167173791+ShanDWanigasuriya@users.noreply.github.com>
2024-08-21 06:53:28 -0700

    Add AboutUs Page and Modify Index Page

--- estateAgents/Controllers/HomeController.cs
+++ estateAgents/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace estateAgents.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
