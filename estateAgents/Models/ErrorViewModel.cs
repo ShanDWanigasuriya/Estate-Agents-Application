058abb0167e8607a916f5f76c712345e2889c791
Shan D. Wanigasuriya <167173791+ShanDWanigasuriya@users.noreply.github.com>
2024-08-21 06:12:30 -0700

    Add project files.

---
+++ estateAgents/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace estateAgents.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
