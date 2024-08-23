19cf4f7badcc42faccbbe0a141ed12b8e4b530cf
Shan D. Wanigasuriya <167173791+ShanDWanigasuriya@users.noreply.github.com>
2024-08-22 22:20:52 -0700

    Modified Register and Login functionalities

--- estateAgents/Data/ApplicationDbContext.cs
+++ estateAgents/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using estateAgents.Areas.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace estateAgents.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
