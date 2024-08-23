f675045830737a4f94dd307120050274913da848
Shan D. Wanigasuriya <167173791+ShanDWanigasuriya@users.noreply.github.com>
2024-08-23 01:52:18 -0700

    Update Project

--- estateAgents/Controllers/HousesController.cs
+++ estateAgents/Controllers/HousesController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using estateAgents.Data;
 using estateAgents.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace estateAgents.Controllers
 {
@@ -19,12 +20,14 @@ namespace estateAgents.Controllers
             _context = context;
         }
 
+        [Authorize]
         // GET: Houses
         public async Task<IActionResult> Index()
         {
             return View(await _context.House.ToListAsync());
         }
 
+        [Authorize]
         // GET: Houses/Details/5
         public async Task<IActionResult> Details(int? id)
         {
@@ -43,12 +46,14 @@ namespace estateAgents.Controllers
             return View(house);
         }
 
+        [Authorize]
         // GET: Houses/Create
         public IActionResult Create()
         {
             return View();
         }
 
+        [Authorize]
         // POST: Houses/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -65,6 +70,7 @@ namespace estateAgents.Controllers
             return View(house);
         }
 
+        [Authorize]
         // GET: Houses/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
@@ -81,6 +87,7 @@ namespace estateAgents.Controllers
             return View(house);
         }
 
+        [Authorize]
         // POST: Houses/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -116,6 +123,7 @@ namespace estateAgents.Controllers
             return View(house);
         }
 
+        [Authorize]
         // GET: Houses/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
@@ -134,6 +142,7 @@ namespace estateAgents.Controllers
             return View(house);
         }
 
+        [Authorize]
         // POST: Houses/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
